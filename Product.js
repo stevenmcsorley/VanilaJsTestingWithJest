4bdee02a91b78c5f3b45bdd66ec9ba161d57b1ca
Steven McSorley <steven.mcsorley@eqtr.com>
2022-06-09 15:52:24 +0100

    undo extends on product error

--- Product.js
+++ Product.js
@@ -5,22 +5,19 @@ class Product {
     this.size = size;
   }
 
+  /// state space expolosion
 
+  filterByColor(products, color) {
+    return products.filter((p) => p.color === color);
+  }
+
+  filterBySize(products, size) {
+    return products.filter((p) => p.size === size);
+  }
+
+  filterByColorAndSize(products, color, size) {
+    return products.filter((p) => p.color === color && p.size === size);
+  }
 }
 
-/// state space expolosion
-class ProductFilter extends Product {
-    filterByColor(products, color) {
-        return products.filter((p) => p.color === color);
-      }
-    
-      filterBySize(products, size) {
-        return products.filter((p) => p.size === size);
-      }
-    
-      filterByColorAndSize(products, color, size) {
-        return products.filter((p) => p.color === color && p.size === size);
-      }
-}
-
-export default ProductFilter;
+export default Product;
